8c9faaad30c3ae7c6bd75d9087fa64e5bc7760fe
Alberta Williams <alberta04@gmail.com>
2021-05-25 21:35:34 -0400

    save email as lowercase

--- backend/user/user.model.js
+++ backend/user/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, lowercase: true },
     password: { type: String, required: true },
 }, { timestamps: true });
 
